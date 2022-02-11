554034ba452b42128e5bbac7be2f3df8065ceb0d
Elena Nikolaevskaya <elena.nikolaevskaya@cosysoft.ru>
2022-02-11 11:42:33 +0400

    90,5

--- test.py
+++ test.py
@@ -2,49 +2,39 @@ import requests
 import base64
 import json
 import io
-import random
 import numpy as np
 from PIL import Image
-from model import get_model
 from keras.models import load_model
 import cv2
 
+IMAGE_SHAPE = (70,70)
 def preprocessing_fun(img):
-#     print(img.shape, img.astype(int))
-#     print(filename)
-#     img = cv2.imread(filename)
-
+    img = img.astype(np.uint8)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.uint8)
-    # gray = img.astype(np.uint8)
-#     img = cv2.fastNlMeansDenoising(gray,None,5,7,21)
-#     img = cv2.fastNlMeansDenoising(gray, None, 3, 7, 21)
-    ret,thresh = cv2.threshold(img,170,255,cv2.THRESH_BINARY)
+    
+    border = 160
+        
+    ret,thresh = cv2.threshold(img,border,255,cv2.THRESH_BINARY)
     kernel = np.ones((5, 5), np.uint8)
-    erode = cv2.erode(thresh, kernel, iterations = 1)
+    erode = cv2.erode(thresh, kernel, iterations = 2)
     img = cv2.bitwise_or(img, erode)
     
-    kernel = np.ones((1, 1), np.uint8)
-    img = cv2.erode(thresh, kernel, iterations = 1)
-    
-    # dec = 60
-    # img = np.where(img < 250, img - dec, img)
-    
-    gray = 255*(img < 128).astype(np.uint8)
+
+    gray = 255*(img < border).astype(np.uint8)
     coords = cv2.findNonZero(gray)
     x, y, w, h = cv2.boundingRect(coords)
     rect = img[y:y+h, x:x+w]
     
-#     img = cv2.copyMakeBorder(rect,top,bottom,right,left,cv2.BORDER_CONSTANT,value=255)
-    img = cv2.copyMakeBorder(rect,5,5,5,5,cv2.BORDER_CONSTANT,value=255)
-    img = cv2.resize(img, (70,70), interpolation = cv2.INTER_NEAREST)
-#     img = cv2.convertScaleAbs(img, alpha=0.5, beta=0)
-#     gray = img.astype(np.uint8)
-#     img = cv2.fastNlMeansDenoising(gray,None,5,7,21)
-#     img = cv2.blur(img, (5, 5))
-    img = img.reshape(70,70,1)
-#     img = crop_white_space(img)
+    img = cv2.copyMakeBorder(rect,1,1,1,1,cv2.BORDER_CONSTANT,value=255)
+    img = cv2.resize(img, IMAGE_SHAPE, interpolation = cv2.INTER_NEAREST)
+    
+
+    img = np.where(img > 80, img - 70, img)
+    img = np.where(img < 0, 0, img)
+    img = img.reshape(*IMAGE_SHAPE,1)
     
     return img
+
 # ФУНКЦИИ ПОЛУЧЕНИЯ И ОТПРАВКИ ДАННЫХ =========================================================================
 
 # получить от сервера данные вида  { "response" : { "data" : { hash : img, ... } }, "error" : error, "code" : code }
@@ -84,9 +74,17 @@ send_url = "https://mooped.net/local/hackathon/api/data/send"
 # user_token = "<user_token>" # токен пользователя
 user_token = "022886bb4d6ee778e07d466b3e42d7c3" # здесь должен быть ВАШ токен пользователя
 
+k = 0
 while True:
     jsn = __getData(get_url, user_token) # запрашиываем данные у сервера и читаем из JSON
     if jsn != None:
+
+        file1 = open('json/json__' + str(k) + '.json', "w")
+        data = json.dumps(jsn, ensure_ascii=False)
+        file1.write(data)
+        file1.close()
+        k = k + 1
+
         data = jsn["response"]["data"]  # извлекаем данные из словаря
         # классифицируем изображения - формируем словарь меток predicted_labels
         predicted_labels = {}
